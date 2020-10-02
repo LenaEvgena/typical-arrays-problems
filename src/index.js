2557540fe178a95056484a0e8f36bdbd44cc54ad
Alena Astravukh <helen151204@gmail.com>
2020-10-02 12:26:11 +0300

    feat: add tab

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 
 exports.min = function min (array) {
-  if (!Array.isArray(array) || !array.length) {
-    return 0;
-  } else {
-    var minResult = array[0];
-    for (var i = 0; i < array.length; i++) {
-      if (array[i] < minResult) {
-        minResult = array[i];
-      }
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    } else {
+        var minResult = array[0];
+        for (var i = 0; i < array.length; i++) {
+        if (array[i] < minResult) {
+            minResult = array[i];
+        }
+        }
     }
-  }
-  return minResult;
+    return minResult;
 }
 
 exports.max = function max (array) {
-  if (!Array.isArray(array) || !array.length) {
-    return 0;
-  } else {
-    var maxResult = array[0];
-    for (var i = 0; i < array.length; i++) {
-      if (array[i] > maxResult) {
-        maxResult = array[i];
-      }
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    } else {
+        var maxResult = array[0];
+        for (var i = 0; i < array.length; i++) {
+        if (array[i] > maxResult) {
+            maxResult = array[i];
+        }
+        }
     }
-  }
-  return maxResult;
+    return maxResult;
 }
 
 exports.avg = function avg (array) {
-  if (!Array.isArray(array) || !array.length) {
-    return 0;
-  } else {
-    var sum = 0;
-    for (var i = 0; i < array.length; i++) {
-      sum += array[i];
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    } else {
+        var sum = 0;
+        for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+        }
+        average = sum / array.length;
     }
-    average = sum / array.length;
-  }
-  return average;
+    return average;
 }
